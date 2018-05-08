6acfb6ccdb31617afed14d5b566cc81a5cce7c59
Alejandro Exposito <alejandro.iglesiasexposito@soprasteria.com>
2018-05-08 15:40:33 +0200

    test.

--- mobile_test/src/test/java/com/appium/test/RunTest.java
+++
@@ -1,7 +0,0 @@
-package com.appium.test;
-
-import com.appium.framework.core.AppiumCukes;
-
-public class RunTest extends AppiumCukes {
-
-}
--- web_test/src/test/java/com/selenium/test/RunTest.java
+++
@@ -1,7 +0,0 @@
-package com.selenium.test;
-
-import com.selenium.framework.core.SeleniumCukes;
-
-public class RunTest extends SeleniumCukes {
-
-}
